ddbdbed79bf83a318e6d9f976fdfac57d21f77c8
tifayuki <tifayuki@gmail.com>
2014-09-08 12:57:16 -0400

    modify `max-open-shards` dynamically`; fix #10

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 set -m
 CONFIG_FILE="/config/config.toml"
 
+#Dynamically change the value of 'max-open-shards' to what 'ulimit -n' returns
+sed -i "s/^max-open-shards.*/max-open-shards = $(ulimit -n)/" ${CONFIG_FILE}
 
 #Configure InfluxDB Cluster
 if [ -n "${FORCE_HOSTNAME}" ]; then
