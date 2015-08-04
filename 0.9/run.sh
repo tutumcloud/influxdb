ec5f12c5b5e8b00795c0f1196faee7ecd4ecfaee
Alexander Heusingfeld <alexander.heusingfeld@innoq.com>
2015-08-04 23:25:14 +0200

    Fixes to make 0.9.2 working
    - updated config.toml for Graphite plugin
    - updated config.toml for UDP
    - added creation of 'admin' user to run.sh
    - commented out seemingly obsolete parts in run.sh

--- 0.9/run.sh
+++ 0.9/run.sh
@@ -17,10 +17,11 @@ if [ -n "${FORCE_HOSTNAME}" ]; then
     fi
 fi
 
-if [ -n "${SEEDS}" ]; then
-    SEEDS=$(eval SEEDS=$SEEDS ; echo $SEEDS | grep '^\".*\"$' || echo "\""$SEEDS"\"" | sed -e 's/, */", "/g')
-    /usr/bin/perl -p -i -e "s/^# seed-servers.*$/seed-servers = [${SEEDS}]/g" ${CONFIG_FILE}
-fi
+# NOTE: 'seed-servers.' is nowhere to be found in config.toml, this cannot work anymore! NEED FOR REVIEW!
+# if [ -n "${SEEDS}" ]; then
+#     SEEDS=$(eval SEEDS=$SEEDS ; echo $SEEDS | grep '^\".*\"$' || echo "\""$SEEDS"\"" | sed -e 's/, */", "/g')
+#     /usr/bin/perl -p -i -e "s/^# seed-servers.*$/seed-servers = [${SEEDS}]/g" ${CONFIG_FILE}
+# fi
 
 if [ -n "${REPLI_FACTOR}" ]; then
     /usr/bin/perl -p -i -e "s/replication-factor = 1/replication-factor = ${REPLI_FACTOR}/g" ${CONFIG_FILE}
@@ -30,30 +31,50 @@ if [ "${PRE_CREATE_DB}" == "**None**" ]; then
     unset PRE_CREATE_DB
 fi
 
-if [ "${SSL_CERT}" == "**None**" ]; then
-    unset SSL_CERT
-fi
-
-if [ "${SSL_SUPPORT}" == "**False**" ]; then
-    unset SSL_SUPPORT
-fi
+# NOTE: It seems this is not used anymore...
+#
+# if [ "${SSL_CERT}" == "**None**" ]; then
+#     unset SSL_CERT
+# fi
+#
+# if [ "${SSL_SUPPORT}" == "**False**" ]; then
+#     unset SSL_SUPPORT
+# fi
 
 # Add Graphite support
 if [ -n "${GRAPHITE_DB}" ]; then
-    sed -i -r -e "/^\s+\[input_plugins.graphite\]/, /^$/ { s/false/true/; s/#//g; s/\"\"/\"${GRAPHITE_DB}\"/g; }" ${CONFIG_FILE}
+    echo "GRAPHITE_DB: ${GRAPHITE_DB}"
+    sed -i -r -e "/^\[\[graphite\]\]/, /^$/ { s/false/true/; s/\"graphitedb\"/\"${GRAPHITE_DB}\"/g; }" ${CONFIG_FILE}
 fi
 
-if [ -n "${GRAPHITE_PORT}" ]; then
-    sed -i -r -e "/^\s+\[input_plugins.graphite\]/, /^$/ { s/2003/${GRAPHITE_PORT}/; }" ${CONFIG_FILE}
+if [ -n "${GRAPHITE_BINDING}" ]; then
+    echo "GRAPHITE_BINDING: ${GRAPHITE_BINDING}"
+    sed -i -r -e "/^\[\[graphite\]\]//, /^$/ { s/\:2003/${GRAPHITE_BINDING}/; }" ${CONFIG_FILE}
+fi
+
+if [ -n "${GRAPHITE_PROTOCOL}" ]; then
+    echo "GRAPHITE_PROTOCOL: ${GRAPHITE_PROTOCOL}"
+    sed -i -r -e "/^\[\[graphite\]\]//, /^$/ { s/\"tcp\"/${GRAPHITE_PROTOCOL}/; }" ${CONFIG_FILE}
+fi
+
+if [ -n "${GRAPHITE_TEMPLATE}" ]; then
+    echo "GRAPHITE_TEMPLATE: ${GRAPHITE_TEMPLATE}"
+    sed -i -r -e "/^\[\[graphite\]\]//, /^$/ { s/\"instance\.profile\.measurement\*\"/${GRAPHITE_TEMPLATE}/; }" ${CONFIG_FILE}
 fi
 
 # Add UDP support
 if [ -n "${UDP_DB}" ]; then
-    sed -i -r -e "/^\s+\[input_plugins.udp\]/, /^$/ { s/false/true/; s/#//g; s/\"\"/\"${UDP_DB}\"/g; }" ${CONFIG_FILE}
+    sed -i -r -e "/^\[udp\]/, /^$/ { s/false/true/; s/#//g; s/\"udpdb\"/\"${UDP_DB}\"/g; }" ${CONFIG_FILE}
 fi
 if [ -n "${UDP_PORT}" ]; then
-    sed -i -r -e "/^\s+\[input_plugins.udp\]/, /^$/ { s/4444/${UDP_PORT}/; }" ${CONFIG_FILE}
+    sed -i -r -e "/^\[udp\]/, /^$/ { s/4444/${UDP_PORT}/; }" ${CONFIG_FILE}
 fi
+
+
+echo "influxdb configuration: "
+cat ${CONFIG_FILE}
+echo "=> Starting InfluxDB ..."
+exec /opt/influxdb/influxd -config=${CONFIG_FILE} &
 
 # Pre create database on the initiation of the container
 API_URL="http://localhost:8086"
@@ -62,8 +83,6 @@ if [ -n "${PRE_CREATE_DB}" ]; then
     if [ -f "/data/.pre_db_created" ]; then
         echo "=> Database had been created before, skipping ..."
     else
-        echo "=> Starting InfluxDB ..."
-        exec /opt/influxdb/influxd -config=${CONFIG_FILE} &
         PASS=${INFLUXDB_INIT_PWD:-root}
         arr=$(echo ${PRE_CREATE_DB} | tr ";" "\n")
 
@@ -71,27 +90,25 @@ if [ -n "${PRE_CREATE_DB}" ]; then
         RET=1
         while [[ RET -ne 0 ]]; do
             echo "=> Waiting for confirmation of InfluxDB service startup ..."
-            sleep 3 
+            sleep 3
             curl -k ${API_URL}/ping 2> /dev/null
             RET=$?
         done
         echo ""
+        echo "=> Creating 'admin' user"
+        /opt/influxdb/influx -host=localhost -port=8086 -execute="CREATE USER admin WITH PASSWORD '${PASS}' WITH ALL PRIVILEGES"
 
         for x in $arr
         do
             echo "=> Creating database: ${x}"
-            /opt/influxdb/influx -host=localhost -port=8086 -username=root -password="${PASS}" -execute="create database \"${x}\""
+            /opt/influxdb/influx -host=localhost -port=8086 -username=admin -password="${PASS}" -execute="create database \"${x}\""
         done
         echo ""
 
         touch "/data/.pre_db_created"
-        fg
-        exit 0
     fi
 else
     echo "=> No database need to be pre-created"
 fi
 
-echo "=> Starting InfluxDB ..."
-
-exec /opt/influxdb/influxd -config=${CONFIG_FILE}
+fg
